1eaade0c5f99a592623952ed26ea9cd06e26f9eb
Zimea <zimea@MacBook-Pro-2.local>
2021-01-31 10:41:18 -0400

    Welcome celebration on the index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>🎉 Next.js Starter!</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="🎉 Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
